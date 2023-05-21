d50ea4d4143c284fc82770593353fabf55882ea2
yanapanlizer <yanapan.t64@rsu.ac.th>
2023-05-22 00:49:24 +0700

    iguigkj

--- index.js
+++ index.js
@@ -23,4 +23,33 @@ connection.query(
 )
 })
 
+app.post('/users', function(request, response) {
+    // Capture the input fields
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exists and are not empty
+    if (username && password) {
+      // Execute SQL query that'll select the account from the database based on the specified username and password
+      connection.query('SELECT * FROM writer WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        // If the account exists
+        if (results.length > 0) {
+          // Authenticate the user
+            //request.session.loggedin = true;
+            //request.session.username = username;
+          // Redirect to home page
+          response.send(results);
+        } else {
+          response.send('Incorrect Username and/or Password!');
+        }     
+        response.end();
+      });
+    } else {
+      response.send('Please enter Username and Password!');
+      response.end();
+    }
+  });
+
+
 app.listen(process.env.PORT || 3000)
